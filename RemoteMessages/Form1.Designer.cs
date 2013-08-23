2c70b17f895c96bac34d1381423c44f95dbef1f1
Aerr <romsahel@gmail.com>
2013-08-23 18:52:35 +0200

    Major corrections (timers repaired, saving drafts more reliable) ; Reselection of the unfocused conversation

--- RemoteMessages/Form1.Designer.cs
+++ RemoteMessages/Form1.Designer.cs
@@ -70,6 +70,7 @@
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "Remote Messages";
+            this.Activated += new System.EventHandler(this.Form1_Activated);
             this.Deactivate += new System.EventHandler(this.Form1_Deactivate);
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
             this.Shown += new System.EventHandler(this.Form1_Shown);
