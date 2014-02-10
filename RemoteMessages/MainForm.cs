b2e70f7ab78024143555e8624d2185f36dadff2a
Aerr <romsahel@gmail.com>
2014-02-10 18:31:42 +0100

    4.0.25 Removed old and unecessary code

--- RemoteMessages/MainForm.cs
+++ RemoteMessages/MainForm.cs
@@ -1389,24 +1389,5 @@ namespace RemoteMessages
         {
             e.Cancel = true;
         }
-
-        const int FEATURE_DISABLE_NAVIGATION_SOUNDS = 21;
-        const int SET_FEATURE_ON_PROCESS = 0x00000002;
-
-        [DllImport("urlmon.dll")]
-        [PreserveSig]
-        [return: MarshalAs(UnmanagedType.Error)]
-        static extern int CoInternetSetFeatureEnabled(
-            int FeatureEntry,
-            [MarshalAs(UnmanagedType.U4)] int dwFlags,
-            bool fEnable);
-
-        static void DisableClickSounds()
-        {
-            CoInternetSetFeatureEnabled(
-                FEATURE_DISABLE_NAVIGATION_SOUNDS,
-                SET_FEATURE_ON_PROCESS,
-                true);
-        }
     }
 }
